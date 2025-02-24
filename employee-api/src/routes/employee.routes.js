4202492f34a1f39da340d5faacd12a4e62957aeb
Gabriela dos Santos <gabisinhas@yahoo.com.br>
2025-02-24 19:32:43 -0300

    crud database setup.

--- employee-api/src/routes/employee.routes.js
+++ employee-api/src/routes/employee.routes.js
@@ -1,6 +1,19 @@
 const router = require('express-promise-router')();
 const employeeController = require('../controllers/employee.controller');
 
-router.post('/employees', employeeController.createEmployee)
+// Rota responsavel para criar novo registro
+router.post('/employees', employeeController.createEmployee);
+
+// Rota para listar todos os funcionatios cadastrados
+router.get('/employees', employeeController.listAllEmployees);
+
+// Procurar funcionário por ID
+router.get('/employees/:id', employeeController.findEmployeeByID);
+
+// Atualizar funcionário por ID
+router.put('/employees/:id', employeeController.updateEmployeeByID);
+
+// Deletar funcionário por ID
+router.delete('/employees/:id', employeeController.deleteEmployeeByID);
 
 module.exports = router;
